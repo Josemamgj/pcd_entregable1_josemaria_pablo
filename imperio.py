8c918afc5627c6601e63f6967fceb75ad857bfdb
Josemamgj <josemagomarizcole@gmail.com>
2026-03-20 20:11:28 +0100

    añadido la clase MiImperio y los metodos de los operarios y comandantes

--- imperio.py
+++ imperio.py
@@ -57,3 +57,44 @@ class Caza_estelar(Nave):
         super().__init__(id_combate, clave, nombre)
         self.dotacion = dotacion
 
+class MiImperio:
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.__almacenes = []
+        self.__naves = []
+
+    def registrar_almacen(self,almacen):
+        self.__almacenes.append(almacen)
+
+    def registrar_nave(self,nave):
+        self.__naves.append(nave)
+
+    #METODOS DEL COMANDANTE
+    def consultar_repuestos(self, nombre_pieza):
+        resultados = []
+        for almacen in self.__almacenes:
+            pieza_encontrada = almacenes.buscar_pieza(nombre_pieza)
+            if pieza_encontrada is not None:
+                resultados.append((almacen.nombre, pieza_encontrada))
+        
+        return resultados
+    
+    def adquirir_repuesto(self,nave,repuesto,cantidad):
+        try:
+            repuesto.usar_stock(cantidad)
+            for _ in range(cantidad):
+                nave.instalar_repuesto(repuesto.nombre)
+            print(f"{cantidad} - {repuesto.nombre} instalados en {nave.nombre}")
+        except StockInsuficienteError as e:
+            print(f"operacion cancelada: {e}")
+
+    #METODOS DEL OPERARIO
+    def mantener_lista_repuestos(self, almacen, repuesto_nuevo):
+        almacen.registrar_pieza_nueva(repuesto_nuevo)
+        print(f"Nuevo repuesto registrado {repuesto_nuevo.nombre} en {almacen.nombre}")
+
+    def atualizar_stocks(self,almacen,repuesto,cantidad_a_añadir):
+        repuesto.añadir_stock(cantidad_a_añadir)
+        print(f"stock de {repuesto.nombre} actualizado, ahora hay {repuesto.ver_cantidad()} unidades")
+
+        
